4ddc459deb904781b1320224fc96d5824716b0d0
vspenkov <82118650+vspenkov@users.noreply.github.com>
2022-02-08 10:38:28 +0300

    Initial commit.

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -178,7 +178,7 @@ APSCHEDULER_RUN_NOW_TIMEOUT = 25  # Seconds
 EMAIL_HOST = 'smtp.yandex.ru'  # адрес сервера Яндекс-почты для всех один и тот же
 EMAIL_PORT = 465  # порт smtp сервера тоже одинаковый
 EMAIL_HOST_USER = 'vladislav.penkov1993'  # ваше имя пользователя, например, если ваша почта user@yandex.ru, то сюда надо писать user, иными словами, это всё то что идёт до собаки
-EMAIL_HOST_PASSWORD = 'Russia2307Messi'  # пароль от почты
+EMAIL_HOST_PASSWORD = '**********'  # пароль от почты
 EMAIL_USE_SSL = True  # Яндекс использует ssl, подробнее о том, что это, почитайте в дополнительных источниках, но включать его здесь обязательно
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER+'@yandex.ru' # здесь указываем уже свою ПОЛНУЮ почту, с которой будут отправляться письма для подтверждения аккаунта
